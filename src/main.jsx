fc56a0ede865953907a133ef54818ffe73233f6f
lydiaagg <agguinilydia@gmail.com>
2026-04-01 21:12:07 +0100

    installation google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,22 @@ import { createRoot } from 'react-dom/client'
 import Home from './pages/Home.jsx'
 import { HelmetProvider } from "react-helmet-async";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import ReactGA from "react-ga4";
+
+// Init Google Analytics
+ReactGA.initialize("G-Q5FMY1S14Z");
+
+// Track pageview
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
-    <SpeedInsights/>
-    <Home />
-  </HelmetProvider>
+  <StrictMode>
+    <HelmetProvider>
+      <Home />
+      <SpeedInsights />
+    </HelmetProvider>
+  </StrictMode>
 )
