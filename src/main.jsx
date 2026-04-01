5116bc57481c8fe571313c296214b0f022a81d67
lydia.agguini <37916742+lydiaagg@users.noreply.github.com>
2026-04-01 21:01:49 +0100

    Change SpeedInsights import from next to react

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Home.jsx'
 import { HelmetProvider } from "react-helmet-async";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <HelmetProvider>
