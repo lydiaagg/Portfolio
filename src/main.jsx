efceb0883cd777764c175bc3f36b720e96babdaa
lydiaagg <agguinilydia@gmail.com>
2026-04-01 20:55:19 +0100

    mise à jour du projet

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Home.jsx'
 import { HelmetProvider } from "react-helmet-async";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <HelmetProvider>
+    <SpeedInsights/>
     <Home />
   </HelmetProvider>
 )
